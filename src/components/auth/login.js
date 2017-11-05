628221b3fcc481d1f4cd8992d7a468214d0ee6ff
Adam Gajzlerowicz <nelf86@gmail.com>
2017-11-05 20:56:10 +0100

    added redux

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TextField from 'material-ui/TextField';
 
-class Login extends Component {
-    render() {
-        return (
-            <div style={{
-                width: 200, padding: 30, margin: '0 auto', textAlign: 'center',
-            }}
-            >
-                <h2>Login</h2>
-                <TextField placeholder="login" style={{ width: '100%' }} />
-                <TextField type="password" placeholder="password" style={{ width: '100%' }} />
-            </div>
-        );
-    }
-}
+const Login = () => (
+    <div style={{
+        width: 200, padding: 30, margin: '0 auto', textAlign: 'center',
+    }}
+    >
+        <h2>Login</h2>
+        <TextField placeholder="login" style={{ width: '100%' }} />
+        <TextField type="password" placeholder="password" style={{ width: '100%' }} />
+    </div>
+);
 
 export default Login;
