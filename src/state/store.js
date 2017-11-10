cca61550d3a2f4f929126dc0491dd276ccff090b
Adam Gajzlerowicz <nelf86@gmail.com>
2017-11-10 08:29:05 +0100

    added sagas

--- src/state/store.js
+++ src/state/store.js
@@ -1,12 +1,22 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-import rootReducer from './reducers/index';
+import rootReducer from './reducers';
+import rootSaga from './sagas';
 
-const middleware = [];
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [sagaMiddleware];
 
 // eslint-disable-next-line
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, undefined, composeEnhancers(applyMiddleware(...middleware)));
+const store = createStore(
+    rootReducer,
+    undefined,
+    composeEnhancers(applyMiddleware(...middleware)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 export {
     store as default,
