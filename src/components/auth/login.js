a04b9db272b6a13fc993c6536ee9d81d6de69e08
Adam Gajzlerowicz <nelf86@gmail.com>
2017-11-11 22:12:10 +0100

    Added frist form

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import TextField from 'material-ui/TextField';
+import { connect } from 'react-redux';
+import { reduxForm, Field } from 'redux-form';
+import Button from 'material-ui/Button';
+import { TextField } from 'redux-form-material-ui';
 
-const Login = () => (
-    <div style={{
-        width: 200, padding: 30, margin: '0 auto', textAlign: 'center',
-    }}
-    >
-        <h2>Login</h2>
-        <TextField placeholder="login" style={{ width: '100%' }} />
-        <TextField type="password" placeholder="password" style={{ width: '100%' }} />
-    </div>
-);
+const LoginForm = ({ handleSubmit }) => {
+    console.log(handleSubmit);
+    return (
+        <div style={{
+            width: 200, padding: 30, margin: '0 auto', textAlign: 'center',
+        }}
+        >
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <Field name="login" placeholder="login" style={{ width: '100%' }} component={TextField} />
+                <Field name="password" type="password" placeholder="password" style={{ width: '100%' }} component={TextField} />
+                <Button raised dense type="submit">
+                    Submit
+                </Button>
+            </form>
+        </div>
+    );
+};
 
-export default Login;
+const FormedLogin = reduxForm({ form: 'login' })(LoginForm);
+const mapState = state => ({
+    foo: 'ba',
+});
+
+const mapDispatch = dispatch => ({
+    onSubmit: (data) => {
+        console.log(data);
+    },
+});
+const ConnectedLogin = connect(mapState, mapDispatch)(FormedLogin);
+
+export default ConnectedLogin;
