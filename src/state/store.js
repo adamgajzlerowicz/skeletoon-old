6bdb3e417e7859bc13241cf5a1e0aa42a2848b32
Adam Gajzlerowicz <nelf86@gmail.com>
2017-11-19 15:02:51 +0100

    udpated root reducer

--- src/state/store.js
+++ src/state/store.js
@@ -1,3 +1,5 @@
+// @flow
+
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
@@ -6,7 +8,8 @@ import createHistory from 'history/createBrowserHistory';
 
 import { reducer as formReducer } from 'redux-form';
 
-import rootReducer from './reducers';
+import type { ActionType } from './type';
+
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -16,12 +19,14 @@ const history = createHistory();
 
 const routerReduxMiddleware = routerMiddleware(history);
 
+// const foo = <T>(state: T, action: ActionType): T => state;
+const foo = (state: {} = {}, action: ActionType): {} => state;
 
 // eslint-disable-next-line
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    combineReducers({ ...rootReducer, router: routerReducer, form: formReducer }),
+    combineReducers({ router: routerReducer, form: formReducer, foo }),
     {},
     composeEnhancers(applyMiddleware(sagaMiddleware, routerReduxMiddleware)),
 );
