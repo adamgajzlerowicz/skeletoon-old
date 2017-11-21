adc0ea1b97777414bada584b7dfe884ccd06f47e
Adam Gajzlerowicz <nelf86@gmail.com>
2017-11-21 18:08:53 +0100

    added redux form saga

--- src/state/sagas/index.js
+++ src/state/sagas/index.js
@@ -1,12 +1,13 @@
 // @flow
 
 import { all } from 'redux-saga/effects';
-
+import formActionSaga from 'redux-form-saga';
 
 // import { saga as loginSaga } from '../ducks/auth';
 
 export default function* rootSaga(): Generator<*, *, *> {
     yield all([
+        formActionSaga(),
         // loginSaga(),
     ]);
 }
